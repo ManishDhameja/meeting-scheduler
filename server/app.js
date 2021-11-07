bb3806690347aea9914f0454789a4ec79c38ab60
jatin831 <jatinbajaj2001@gmail.com>
2021-11-07 13:48:06 +0530

    connection to mongodb

--- server/app.js
+++ server/app.js
@@ -1,26 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,13 +14,21 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.log(err);
+  res.status(err.statusCode || 500).json({message: err.message});
 });
 
-module.exports = app;
+// Connecting to mongodb
+mongoose.connect('mongodb+srv://devninjas:devninjas@devninjas.yukd1.mongodb.net/test', 
+{ 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+})
+.then(result => {
+    app.listen(process.env.PORT || 5000);
+    console.log("Server started at port 5000");
+})
+.catch(err => {
+    console.log(err);
+})
