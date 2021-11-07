49df8c6dfc17bc0e171580b81400031a0c540cd6
Rishab Goyal <rishabgoyal432@gmail.com>
2021-11-07 19:51:42 +0530

    is auth middleware get users

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const authRouter = require('./routes/auth');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authRouter);
+app.use('/users', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const authController = require('../controllers/auth');
+const authController = require('../controllers/authController');
 
-router.post('/signup', authController.signup)
+router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 router.post('/verifyToken', authController.verifyToken);
 
