09c46dc77ef5135f85b2150760e42b5ed3abcd4a
jatin831 <jatinbajaj2001@gmail.com>
2021-11-07 21:00:32 +0530

    file name changed

--- client/src/components/Sidedrawer/Sidedrawer.jsx
+++ client/src/components/Sidedrawer/Sidedrawer.jsx
@@ -4,8 +4,8 @@ import './Sidedrawer.css';
 const Sidedrawer =()=>{
     return(
         <>
-        <h1>Hello</h1>
-        <h1>Rolit</h1>
+            <h1>Hello</h1>
+            <h1>Rolit</h1>
         </>
     );
 }
