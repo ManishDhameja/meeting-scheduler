c986272f7b9fe89df404f78167e378c4e48dec5e
Rishab Goyal <rishabgoyal432@gmail.com>
2021-11-07 18:29:01 +0530

    user schema update

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-        name: {
-            type: String,
-            required: true
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true 
-        },
-        password: {
-            type: String,
-            required: true
-        }
-    },
-    {
-        timestamps: true
-    }
-)
+const hoursSchema = new Schema({
+  startTime: {
+    type : Date,
+    required: true
+  },
+  endTime: {
+    type : Date,
+    required: true
+  }
+},{
+  timestamps : true
+});
 
-module.exports = mongoose.model('User', userSchema);
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    workingHours: [hoursSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
