9da529f0478c2d850e075b26bb74bbbfe06e1fe7
Rolit19 <rolit454@gmail.com>
2021-11-08 10:38:35 +0530

    Header redefined

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import './Header.css';
+import { FcCalendar } from 'react-icons/fc';
+import { ImMenu } from 'react-icons/im';
+import { IconContext } from "react-icons";
+import { BiChevronLeft } from 'react-icons/bi';
+import { BiChevronRight } from 'react-icons/bi';
+
+const Header = () => {
+    return (
+        <>
+            <div className="HeaderContainer">
+                <div className="Header">
+                    <ImMenu style={{fontSize:"1.8rem"}} />
+                    <div style={{marginLeft:"50px", marginRight:"50px"}}>
+                        <FcCalendar style={{fontSize:"2.4rem"}} />
+                        <span className="HeaderTitle">Meeting Scheduler</span>
+                    </div>
+                    <div className="Headersubpart">
+                        <button className="HeaderTodayBtn DateButton">
+                            Today
+                        </button>
+                        <div className="HeaderNavDateBtns">
+                            <IconContext.Provider value={{className: "HeaderNavDateIcons"}}>
+                            <BiChevronLeft  />
+                            <BiChevronRight />
+                            </IconContext.Provider>
+                        </div>
+                        <span className="HeaderDateselected">
+                            08-11-2021
+                        </span>
+                    </div>
+                    <div className="HeaderSelector">
+                        <select className="HeaderDropdown">
+                            <option value="day">Day</option>
+                            <option value="week">Week</option>
+                            <option value="month">Month</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
