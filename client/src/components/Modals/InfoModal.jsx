f03e5de822a010bdac9670533e1bbfdae63174a6
Rolit19 <rolit454@gmail.com>
2021-11-08 17:45:08 +0530

    modal added

--- client/src/components/Modals/InfoModal.jsx
+++ client/src/components/Modals/InfoModal.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 const InfoModal = () => {
-    
-
+    const list = [
+        {
+            startTime: 1636362006000,
+            endTime: 1636369206000,
+            title: "Class Bunk",
+            decription: "Lets do it boys",
+            type: "accepted",
+            meetingLink: "https://meet.google.com/zpr-tczz-qnu",
+        }
+    ];
     return (
-        <div>
-            hello
-        </div>
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header className="Modal-header">
+                    <Modal.Title>Meeting Details</Modal.Title>
+                    <IconButton onClick={handleClose}><ClearIcon /></IconButton>
+                </Modal.Header>
+                <Modal.Body>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose} className="Modal-button">
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+        </>
     )
 }
 
